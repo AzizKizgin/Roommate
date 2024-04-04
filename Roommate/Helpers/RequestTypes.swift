3de0ed66ae949d4926593733fcce4bec37b93e7a
Aziz Kızgın <azizkzgn@gmail.com>
2024-04-04 21:15:19 +0300

    feat: RequestType added

---
+++ Roommate/Helpers/RequestTypes.swift
@@ -0,0 +1,15 @@
+//
+//  RequestTypes.swift
+//  Roommate
+//
+//  Created by Aziz Kızgın on 4.04.2024.
+//
+
+import Foundation
+
+enum RequestType: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
