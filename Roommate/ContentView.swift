f718025607a2130983be32a6e5c17567e946f2a4
Aziz Kızgın <azizkzgn@gmail.com>
2024-04-04 18:18:42 +0300

    Initial Commit

---
+++ Roommate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Roommate
+//
+//  Created by Aziz Kızgın on 4.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Roommate/RoommateApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoommateApp.swift
+//  Roommate
+//
+//  Created by Aziz Kızgın on 4.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct RoommateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
