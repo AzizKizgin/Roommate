1154738f16c58f29c165cbeaf6aad8f611602d8d
Aziz Kızgın <azizkzgn@gmail.com>
2024-04-04 21:14:46 +0300

    feat: Extensions added

---
+++ Roommate/Helpers/Extensions.swift
@@ -0,0 +1,32 @@
+//
+//  Extensions.swift
+//  Roommate
+//
+//  Created by Aziz Kızgın on 4.04.2024.
+//
+
+import Foundation
+
+extension Encodable {
+    func toData() -> Data?{
+        let encoder = JSONEncoder()
+        do {
+            let data = try encoder.encode(self)
+            return data
+        }
+        catch {
+            return nil
+        }
+    }
+    
+    func toJSONString() -> String {
+        let encoder = JSONEncoder()
+        do {
+            let data = try encoder.encode(self)
+            return String(data: data, encoding: .utf8)!
+        }
+        catch {
+            return ""
+        }
+    }
+}
