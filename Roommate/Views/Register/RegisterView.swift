59b88279994c12ec133cc6909fc4bab0f82b5941
Aziz Kızgın <azizkzgn@gmail.com>
2024-04-07 00:13:15 +0300

    feat: register screen added

--- Roommate/Views/Register/RegisterView.swift
+++ Roommate/Views/Register/RegisterView.swift
@@ -8,11 +8,56 @@
 import SwiftUI
 
 struct RegisterView: View {
+    @StateObject private var registerVM = RegisterViewModel()
+    @Environment(\.dismiss) private var dismiss
+    
+    func get18YearsAgo() -> Date{
+        let currentDate = Date()
+        let calendar = Calendar.current
+        
+        if let date = calendar.date(byAdding: .year, value: -18, to: currentDate) {
+            return date
+        } else {
+            return .now
+        }
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView{
+            VStack(spacing:15){
+                TextField("First Name", text: $registerVM.user.firstName)
+                    .capsuleTextField(icon: "person.circle.fill")
+                TextField("Last Name", text: $registerVM.user.lastName)
+                    .capsuleTextField(icon: "person.circle.fill")
+                TextField("Email", text: $registerVM.user.email)
+                    .capsuleTextField(icon: "person.circle.fill")
+                DateInput(date: $registerVM.user.birthDate)
+                TextField("Job", text: $registerVM.user.job)
+                    .capsuleTextField(icon: "person.circle.fill")
+                TextField("Phone", text: $registerVM.user.phoneNumber)
+                    .capsuleTextField(icon: "person.circle.fill")
+                TextField("Password", text: $registerVM.user.password)
+                    .capsuleTextField(icon: "person.circle.fill")
+                TextField("Confirm Password", text: $registerVM.confirmPassword)
+                    .capsuleTextField(icon: "person.circle.fill")
+                FormButton(title: "Register", onPress: registerVM.registerUser)
+                    .padding(.vertical)
+            }
+            .padding()
+            .alert(registerVM.errorText, isPresented: $registerVM.showError){
+                Button("Okay", role: .cancel) {}
+            }
+            .alert("Your account has been created", isPresented: $registerVM.isSuccess){
+                Button("Okay", role: .cancel) {
+                    dismiss()
+                }
+            }
+        }
+        .navigationTitle("Register")
     }
 }
 
 #Preview {
-    RegisterView()
+    NavigationStack{
+        RegisterView()
+    }
 }
--- Roommate/Views/Register/RegisterViewModel.swift
+++ Roommate/Views/Register/RegisterViewModel.swift
@@ -6,3 +6,74 @@
 //
 
 import Foundation
+
+class RegisterViewModel: ObservableObject {
+    @Published var user: UserRegisterInfo = UserRegisterInfo()
+    @Published var confirmPassword: String = ""
+    @Published var showError: Bool = false
+    @Published var errorText: String = ""
+    @Published var isLoading: Bool = false
+    @Published var isSuccess: Bool = false
+    
+    // MARK: - Register
+    func registerUser() {
+        isLoading = true
+        guard validateFields() else {
+            isLoading = false
+            return
+        }
+        UserManager.shared.register(registerData: user) { [weak self] result in
+            defer {
+                DispatchQueue.main.async {
+                    self?.isLoading = false
+                }
+            }
+            DispatchQueue.main.async {
+                switch result {
+                case .success:
+                    self?.isSuccess.toggle()
+                case .failure(let error):
+                    self?.handleAuthError(error.localizedDescription)
+                }
+            }
+        }
+    }
+    
+    // MARK: - Set error
+    private func setFieldError(_ message: String){
+        self.showError.toggle()
+        self.errorText = message
+    }
+    
+    private func handleAuthError(_ error: String){
+        showError.toggle()
+        errorText = error
+    }
+    
+    // MARK: - Field Validation
+    private func validateFields() -> Bool{
+        if user.firstName.isEmpty 
+            || user.lastName.isEmpty
+            || user.email.isEmpty
+            || user.password.isEmpty 
+            || user.job.isEmpty
+            || user.phoneNumber.isEmpty
+            || confirmPassword.isEmpty {
+            setFieldError("Please fill in all fields")
+            return false
+        }
+        else if !Utils.isValidEmail(for: user.email) {
+            setFieldError("Your email is invalid")
+            return false
+        }
+        else if !Utils.isPasswordValid(for: user.password){
+            setFieldError("The password must contain at least 8 characters, 1 special character and 1 uppercase letter.")
+            return false
+        }
+        else if user.password != confirmPassword {
+            setFieldError("Passwords do not match")
+            return false
+        }
+        return true
+    }
+}
