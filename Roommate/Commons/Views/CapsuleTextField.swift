0364e61a1b712f7cd039c447c105fd0194abb04d
Aziz Kızgın <azizkzgn@gmail.com>
2024-04-07 00:11:49 +0300

    feat: CapsuleTextField added

--- Roommate/Commons/Views/CapsuleTextField.swift
+++ Roommate/Commons/Views/CapsuleTextField.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct CapsuleTextField: ViewModifier {
+    let icon: String
+    @AppStorage("isDark") private var isDark: Bool = false
+    func body(content: Content) -> some View {
+        HStack(alignment:.top){
+            Image(systemName: icon)
+                .font(.title2)
+                .foregroundStyle(Color.accentColor)
+            content
+        }
+        .padding()
+        .background{
+            RoundedRectangle(cornerRadius: 10, style: .continuous)
+                .foregroundStyle(.gray.opacity(isDark ? 0.25: 0.15))
+        }
+    }
+}
+
+extension View {
+    func capsuleTextField(icon: String) -> some View {
+        self.modifier(CapsuleTextField(icon: icon))
+    }
+}
+
+#Preview {
+    TextField("Text", text: .constant(""))
+        .capsuleTextField(icon: "checkmark.circle.fill")
+}
