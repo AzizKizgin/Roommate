3d20d19cfd1a8d04ccc48526c6728ed3e7dd8768
Aziz Kızgın <azizkzgn@gmail.com>
2024-04-15 22:29:44 +0300

    feat: FormInput added

--- Roommate/Commons/Views/FormInput.swift
+++ Roommate/Commons/Views/FormInput.swift
@@ -8,11 +8,72 @@
 import SwiftUI
 
 struct FormInput: View {
+    @AppStorage("isDark") private var isDark: Bool = false
+    @State private var showContent: Bool = false
+    @Binding var text: String
+    let title: LocalizedStringKey
+    let icon: String
+    var isMultiline: Bool = false
+    var isSecureText: Bool = false
+    
+    init(_ title: LocalizedStringKey, text: Binding<String>, icon: String) {
+        self._text = text
+        self.title = title
+        self.icon = icon
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(alignment:isMultiline ? .top : .center){
+            Image(systemName: icon)
+                .font(.title3)
+                .foregroundStyle(Color.accentColor)
+            if isMultiline {
+                TextField(title, text: $text, axis: .vertical)
+                    .lineLimit(5...10)
+            }
+            else if !isSecureText {
+                TextField(title, text: $text)
+            }
+            else {
+                HStack{
+                    if showContent {
+                        TextField(title, text: $text)
+                    }
+                    else {
+                        SecureField(title, text: $text)
+                    }
+                    Image(systemName:showContent ? "eye.fill": "eye.slash.fill")
+                        .foregroundStyle(.accent)
+                        .onTapGesture {
+                            showContent.toggle()
+                        }
+                }
+            }
+        }
+        .disableAutocorrection(true)
+        .autocapitalization(.none)
+        .frame(height: isMultiline ? nil : 25)
+        .padding()
+        .background{
+            RoundedRectangle(cornerRadius: 10, style: .continuous)
+                .foregroundStyle(.gray.opacity(isDark ? 0.25: 0.15))
+        }
+    }
+}
+
+extension FormInput {
+    func secureText() ->  FormInput  {
+        var copy = self
+        copy.isSecureText = true
+        return copy
+    }
+    func multiline() ->  FormInput  {
+        var copy = self
+        copy.isMultiline = true
+        return copy
     }
 }
 
 #Preview {
-    FormInput()
+    FormInput("Title",text: .constant("text"), icon: "house")
+        .secureText()
 }
