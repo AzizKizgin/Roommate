75274637f8c659613ff342d80c5f82d2e983db68
Aziz Kızgın <azizkzgn@gmail.com>
2024-04-15 23:08:18 +0300

    feat: Room Create Details View added

--- Roommate/Views/CreateRoom/RoomCreateDetailsView.swift
+++ Roommate/Views/CreateRoom/RoomCreateDetailsView.swift
@@ -9,10 +9,56 @@ import SwiftUI
 
 struct RoomCreateDetailsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView{
+            VStack(spacing: 50){
+                VStack(spacing: 15){
+                    Text("Address")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .foregroundStyle(.accent)
+                    FormInput("Street", text: .constant(""), icon: "map.circle.fill")
+                    HStack{
+                        FormInput("Apartment No", text: .constant(""), icon: "map.circle.fill")
+                        FormInput("Building No", text: .constant(""), icon: "map.circle.fill")
+                    }
+                    HStack{
+                        FormInput("Town", text: .constant(""), icon: "map.circle.fill")
+                        FormInput("ZipCode", text: .constant(""), icon: "map.circle.fill")
+                    }
+                    HStack{
+                        FormInput("City", text: .constant(""), icon: "map.circle.fill")
+                        FormInput("Country", text: .constant(""), icon: "map.circle.fill")
+                    }
+                }
+                VStack(spacing: 15){
+                    Text("Room")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .foregroundStyle(.accent)
+                    HStack{
+                        FormInput("Price", text: .constant(""), icon: "turkishlirasign")
+                        FormInput("Size", text: .constant(""), icon: "ruler.fill")
+                    }
+                    HStack{
+                        FormInput("Room", text: .constant(""), icon: "bed.double.fill")
+                        FormInput("Bath", text: .constant(""), icon: "bathtub.fill")
+                    }
+                    FormInput("About Room", text: .constant(""), icon: "info.circle.fill")
+                        .multiline()
+                }
+            }
+            .padding()
+        }
+        .toolbar{
+            ToolbarItem(placement: .confirmationAction) {
+                Button("Save") {
+                    
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    RoomCreateDetailsView()
+    NavigationStack{
+        RoomCreateDetailsView()
+    }
 }
