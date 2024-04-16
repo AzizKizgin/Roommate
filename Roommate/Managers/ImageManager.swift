312bfc3a2e3414eca69de27253e1a7c18978a189
Aziz Kızgın <azizkzgn@gmail.com>
2024-04-17 02:13:44 +0300

    feat: convertStringToImageData added

--- Roommate/Managers/ImageManager.swift
+++ Roommate/Managers/ImageManager.swift
@@ -14,7 +14,7 @@ class ImageManager {
     private init() {}
     
     func convertImageToString(for img: UIImage, completion: @escaping (String?) -> Void) {
-        DispatchQueue.global().async {
+        DispatchQueue.main.async {
             let imageData = img.jpegData(compressionQuality: 1)
             let base64String = imageData?.base64EncodedString()
             DispatchQueue.main.async {
@@ -29,5 +29,14 @@ class ImageManager {
             return image
         }
         return nil
+    }
+    
+    func convertStringToImageData(for imgString: String, completion: @escaping (Data?) -> Void) {
+        Task {
+            if let imageData = Data(base64Encoded: imgString) {
+                completion(imageData)
+            }
+            completion(nil)
+        }
     }
 }
