8faa3d7d9b1f2d6e441a6938817fb6005d896178
Aziz Kızgın <azizkzgn@gmail.com>
2024-04-18 23:38:06 +0300

    feat: CreateRoomView navigationDestination

--- Roommate/Views/MainView/MainView.swift
+++ Roommate/Views/MainView/MainView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct MainView: View {
     @State private var selectedTab: TabScreen = TabScreen.Home
+    @State private var showAddRoom: Bool = false
     var body: some View {
         TabView(selection: $selectedTab) {
             HomeView()
@@ -33,12 +34,17 @@ struct MainView: View {
                     Label("Account", systemImage: "person")
                 }
         }
+        .navigationDestination(isPresented: $showAddRoom, destination: {
+            CreateRoomView()
+        })
         .toolbar {
             switch selectedTab {
             case TabScreen.Home:
-                Image(systemName: "plus.circle.fill")
-                    .font(.title2)
-                    .foregroundStyle(.accent)
+                Button(action: {showAddRoom.toggle()}, label: {
+                    Image(systemName: "plus.circle.fill")
+                        .font(.title2)
+                        .foregroundStyle(.accent)
+                })
             default:
                 EmptyView()
             }
