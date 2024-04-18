4bb4848c469fc2944a3ad9f97c8196ac8accf5ac
Aziz Kızgın <azizkzgn@gmail.com>
2024-04-18 23:37:45 +0300

    feat: RoomDetailView added

---
+++ Roommate/Views/RoomDetail/RoomDetailViewModel.swift
@@ -0,0 +1,41 @@
+//
+//  RoomDetailViewModel.swift
+//  Roommate
+//
+//  Created by Aziz Kızgın on 17.04.2024.
+//
+
+import Foundation
+
+@Observable class RoomDetailViewModel {
+    var room: RoomProtocol
+    var isLoading: Bool = false
+    
+    init(room: RoomProtocol) {
+         self.room = room
+     }
+    
+    func setRoom(room: RoomProtocol) {
+        self.room = room
+    }
+    
+    func favoriteRoom() {
+        self.isLoading = true
+        RoomManager.shared.favoriteRoom(id: room.id) { [weak self] result in
+            defer {
+                DispatchQueue.main.async {
+                    self?.isLoading = false
+                }
+            }
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let room):
+                    self?.room = room
+                    print("sss")
+                case .failure(let error):
+                    print(error)
+                }
+            }
+        }
+    }
+}
