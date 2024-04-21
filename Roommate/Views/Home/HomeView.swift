ed2a31d2fd2f6c9144418686603e53ae6c45ded1
Aziz Kızgın <azizkzgn@gmail.com>
2024-04-22 01:38:24 +0300

    feat: Homeview updated

--- Roommate/Views/Home/HomeView.swift
+++ Roommate/Views/Home/HomeView.swift
@@ -8,11 +8,61 @@
 import SwiftUI
 
 struct HomeView: View {
+    @Bindable private var homeVM = HomeViewModel()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView{
+            ForEach(homeVM.rooms, id: \.id) { room in
+                RoomItem(room: room) 
+                    .onAppear{
+                        homeVM.loadMoreContent(currentItem: room)
+                    }
+            }
+        }
+        .refreshable {
+            homeVM.refresh()
+        }
+        .alert(self.homeVM.errorText, isPresented: $homeVM.showError, actions: {
+            Button("Okay") {}
+        })
+        .onAppear {
+            DispatchQueue.main.async {
+                homeVM.isActive = true
+            }   
+        }
+        .onDisappear {
+            DispatchQueue.main.async { 
+                homeVM.isActive = false
+            }
+        }
+        .toolbar {
+            if homeVM.isActive {
+                ToolbarItem(placement: .topBarLeading) {
+                    NavigationLink {
+                        FilterSelectionView(queryObject: $homeVM.queryObject)
+                    }
+                label: {
+                    Image(systemName: "line.3.horizontal.decrease.circle.fill")
+                        .font(.title2)
+                        .foregroundStyle(.accent)
+                }
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    NavigationLink {
+                        CreateRoomView()
+                    } label: {
+                        Image(systemName: "plus.circle.fill")
+                            .font(.title2)
+                            .foregroundStyle(.accent)
+                    }
+                }
+            }
+        }
+        
     }
 }
 
 #Preview {
-    HomeView()
+    NavigationStack {
+        HomeView()
+    }
 }
--- Roommate/Views/Home/HomeViewModel.swift
+++ Roommate/Views/Home/HomeViewModel.swift
@@ -6,3 +6,60 @@
 //
 
 import Foundation
+import SwiftUI
+
+@Observable class HomeViewModel {
+    var rooms: [Room] = []
+    var isLoading: Bool = false
+    var showError: Bool = false
+    var errorText: LocalizedStringKey = ""
+    var isActive: Bool = false
+    var queryObject: RoomQueryObject = RoomQueryObject()
+    var totalPage:Int = 0
+    
+    func loadMoreContent(currentItem item: Room){
+        let thresholdIndex = self.rooms.index(self.rooms.endIndex, offsetBy: -1)
+        if thresholdIndex == item.id, (queryObject.page + 1) <= totalPage {
+            queryObject.page += 1
+            getRooms()
+        }
+    }
+    
+    init () {
+        getRooms()
+    }
+    
+    func refresh() {
+        self.rooms = []
+        self.totalPage = 0
+        self.queryObject.page = 1
+        getRooms()
+    }
+    
+    func getRooms() {
+        isLoading = true
+        RoomManager.shared.getRooms(query: self.queryObject) { [weak self] result in
+            defer {
+                self?.isLoading = false
+            }
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let response):
+                    print("\(response.rooms.count)")
+                    self?.rooms.append(contentsOf: response.rooms)
+                    self?.totalPage = response.totalCount
+                case .failure(let error):
+                    print(error)
+                    self?.setError("An error occurred while fetching data")
+                    print("An error occurred while fetching data: \(error.localizedDescription)")
+                }
+            }
+        }
+    }
+    
+    private func setError(_ message: LocalizedStringKey){
+        self.showError.toggle()
+        self.errorText = message
+    }
+    
+}
