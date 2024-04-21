7f4425de0102e7994bb79728b36815cf8be44e04
Aziz Kızgın <azizkzgn@gmail.com>
2024-04-22 01:39:58 +0300

    feat: RoomItem added

--- Roommate/Commons/Views/RoomItem/RoomItem.swift
+++ Roommate/Commons/Views/RoomItem/RoomItem.swift
@@ -8,11 +8,36 @@
 import SwiftUI
 
 struct RoomItem: View {
+    private var idiom : UIUserInterfaceIdiom { UIDevice.current.userInterfaceIdiom }
+    let room: RoomProtocol
+    @State private var imageDatas: [Data] = []
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            RoomImages(imageDatas: imageDatas)
+                .frame(height: idiom == .pad ? 500 :270)
+            VStack(spacing: 10){
+                RoomAddressInfo(address: room.address)
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                Divider()
+                RoomProps(price: room.price, size: room.size, roomCount: room.roomCount, bathCount: room.bathCount)
+            }
+            .padding()
+        }
+        .onAppear{
+            Task {
+                if !room.images.isEmpty, imageDatas.isEmpty, let firstImage = room.images.first {
+                    ImageManager.shared.convertStringToImageData(for: firstImage, completion: { data in
+                        if let data {
+                            self.imageDatas.append(data)
+                        }
+                    })
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    RoomItem()
+    RoomItem(room: testRoom)
 }
