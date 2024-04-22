57312272cfc6cb86c016fc2786c7948c5e772b1b
Aziz Kızgın <azizkzgn@gmail.com>
2024-04-22 18:40:32 +0300

    feat: screen updated

--- Roommate/Views/Home/HomeView.swift
+++ Roommate/Views/Home/HomeView.swift
@@ -9,13 +9,36 @@ import SwiftUI
 
 struct HomeView: View {
     @Bindable private var homeVM = HomeViewModel()
+    @State private var selectedRoom: RoomProtocol?
+    @State private var showFilters: Bool = false
     var body: some View {
         ScrollView{
-            ForEach(homeVM.rooms, id: \.id) { room in
-                RoomItem(room: room) 
+            VStack {
+                if homeVM.rooms.isEmpty {
+                    VStack {
+                        Text("No data")
+                    }
+                }
+                ForEach(homeVM.rooms, id: \.id) { room in
+                    NavigationLink(value: room){
+                        RoomItem(room: room)
+                            .contentShape(Rectangle())
+                    }
+                    .buttonStyle(.plain)
                     .onAppear{
                         homeVM.loadMoreContent(currentItem: room)
                     }
+                    .padding(.vertical)
+                }
+            }
+            .padding()
+        }
+        .onChange(of: homeVM.queryObject, { oldValue, newValue in
+            homeVM.refresh()
+        })
+        .onAppear {
+            if homeVM.rooms.isEmpty {
+                homeVM.getRooms()
             }
         }
         .refreshable {
@@ -24,40 +47,34 @@ struct HomeView: View {
         .alert(self.homeVM.errorText, isPresented: $homeVM.showError, actions: {
             Button("Okay") {}
         })
-        .onAppear {
-            DispatchQueue.main.async {
-                homeVM.isActive = true
-            }   
-        }
-        .onDisappear {
-            DispatchQueue.main.async { 
-                homeVM.isActive = false
-            }
-        }
         .toolbar {
-            if homeVM.isActive {
-                ToolbarItem(placement: .topBarLeading) {
-                    NavigationLink {
-                        FilterSelectionView(queryObject: $homeVM.queryObject)
-                    }
+            ToolbarItem(placement: .topBarLeading) {
+                Button {
+                    showFilters.toggle()
+                }
                 label: {
                     Image(systemName: "line.3.horizontal.decrease.circle.fill")
                         .font(.title2)
                         .foregroundStyle(.accent)
                 }
-                }
-                ToolbarItem(placement: .topBarTrailing) {
-                    NavigationLink {
-                        CreateRoomView()
-                    } label: {
-                        Image(systemName: "plus.circle.fill")
-                            .font(.title2)
-                            .foregroundStyle(.accent)
-                    }
+            }
+            ToolbarItem(placement: .topBarTrailing) {
+                NavigationLink {
+                    CreateRoomView()
+                } label: {
+                    Image(systemName: "plus.circle.fill")
+                        .font(.title2)
+                        .foregroundStyle(.accent)
                 }
             }
+            
         }
-        
+        .fullScreenCover(isPresented: $showFilters, content: {
+            FilterSelectionView(queryObject: $homeVM.queryObject)
+        })
+        .navigationDestination(for: Room.self, destination: { room in
+            RoomDetailView(room: room)
+        })
     }
 }
 
